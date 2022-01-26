46d00a3ac79b9b72318518770bbc2b22a0922c75
boltaev <lazizbold@gmail.com>
2022-01-27 01:10:31 +0500

    checking input word from file and output true or false

--- main.go
+++ main.go
@@ -5,28 +5,42 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	var fileName string
+	// scanning file name and taboo word from
+	// command line
+	var fileName, tabooWord string
+	var isTaboo bool
 	fmt.Scan(&fileName)
+	fmt.Scan(&tabooWord)
+	tabooWord = strings.ToLower(tabooWord)
 
+	// opening file and checking existing test
+	// or return error message, defer close function
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
+	// assigning scanning function to the scanner variable
 	scanner := bufio.NewScanner(file)
 
 	// split each scanned line into words
 	scanner.Split(bufio.ScanWords)
 
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		if tabooWord == scanner.Text() {
+			isTaboo = true
+		}
 	}
+	fmt.Println(isTaboo)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 }
+
+// recommendations and hits
