7b242f612250890e4ec6ad78ba4b18d2b4d0424e
boltaev <lazizbold@gmail.com>
2022-01-31 02:22:30 +0500

    sentences input possible but not working yet

--- main.go
+++ main.go
@@ -8,32 +8,45 @@ import (
 	"strings"
 )
 
-func scanTaboo() string {
+func scanTaboo() []string {
 	var tabooWord string
 	fmt.Scan(&tabooWord)
-	tabooWord = strings.ToLower(tabooWord) // lowercase the string
-	return tabooWord
+	inputArr := strings.Split(tabooWord, " ")
+	return inputArr
 }
 
-func remarkTaboo(scanner *bufio.Scanner, tabooWord string) {
-	for scanner.Scan() {
-		if tabooWord == scanner.Text() {
-			stars := len(tabooWord)
-			tabooWord = ""
-			for i := 0; i < stars; i++ {
-				tabooWord += "*"
+func remarkTaboo(badWords []string, tabooWord []string) {
+	var result string
+	inputArr := tabooWord // here assigning new variable for case
+	// lowering words
+	for _, s := range inputArr {
+		s = strings.ToLower(s)
+	}
+	// comparing words from two arrays
+	for i := 0; i < len(badWords); i++ {
+		for j := 0; j < len(inputArr); j++ {
+			if badWords[i] == inputArr[j] {
+				stars := len(inputArr[j])
+				tabooWord[j] = ""
+				for i := 0; i < stars; i++ {
+					tabooWord[j] += "*"
+				}
 			}
 		}
 	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	// uniting words from arrays to the string
+	for q := 0; q < len(tabooWord); q++ {
+		result += tabooWord[q] + " "
 	}
-	fmt.Println(tabooWord)
+
+	fmt.Println(result)
 }
 
 func main() {
 	// scanning file name from command line
-	var fileName, tabooWord string
+	var fileName string
+	var badWords, tabooWord []string
+
 	fmt.Scan(&fileName)
 	// opening file and checking existing test
 	// or return error message, defer close function
@@ -49,12 +62,20 @@ func main() {
 	// split each scanned line into words
 	scanner.Split(bufio.ScanWords)
 
+	for i := 0; i < len(scanner.Text()); i++ {
+		badWords[i] = scanner.Text()
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
 	for {
 		tabooWord = scanTaboo()
-		if tabooWord == "exit" {
+		if tabooWord[0] == "exit" {
 			fmt.Println("Bye!")
 			break
 		}
-		remarkTaboo(scanner, tabooWord)
+		remarkTaboo(badWords, tabooWord)
 	}
 }
